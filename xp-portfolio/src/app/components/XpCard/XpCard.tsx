ad87e04a490fc74b59d09de5563d36019cfe1f31
km3337 <kmentor25@gmail.com>
2024-09-02 17:10:50 -0400

    added hover/motion div to xp card

--- xp-portfolio/src/app/components/XpCard/XpCard.tsx
+++ xp-portfolio/src/app/components/XpCard/XpCard.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import "xp.css/dist/XP.css";
 import styles from './XpCard.module.css'
+import { motion } from "framer-motion"
+
 
 export interface XpCardProps {
     title: string,
@@ -13,23 +15,30 @@ export interface XpCardProps {
 // TODO: add framer motion hover. add redirect on help button click. add logic to hide card on close click.
 export const XpCard = ({ title, children, description, date, dimensionX }: XpCardProps) => {
     return (
-        <div className={styles.XpCardWrapper} style={{ width: dimensionX }}>
-            <div className="window" style={{ width: dimensionX }}>
-                <div className="title-bar">
-                    <div className="title-bar-text">{title}</div>
-                    <div className="title-bar-controls">
-                        <button aria-label="Help"></button>
-                        <button aria-label="Close"></button>
+        <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            whileHover={{ scale: 1.1 }}
+        >
+            <div className={styles.XpCardWrapper} style={{ width: dimensionX }}>
+                <div className="window" style={{ width: dimensionX }}>
+                    <div className="title-bar">
+                        <div className="title-bar-text">{title}</div>
+                        <div className="title-bar-controls">
+                            <button aria-label="Help"></button>
+                            <button aria-label="Close"></button>
+                        </div>
+                    </div>
+                    <div className="window-body">
+                        {children}
+                    </div>
+                    <div className="status-bar">
+                        <p className="status-bar-field">{description}</p>
+                        <p className="status-bar-field">{date}</p>
                     </div>
                 </div>
-                <div className="window-body">
-                    {children}
-                </div>
-                <div className="status-bar">
-                    <p className="status-bar-field">{description}</p>
-                    <p className="status-bar-field">{date}</p>
-                </div>
-            </div>
-        </div >
+            </div >
+        </motion.div>
     )
 }
